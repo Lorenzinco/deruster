d9f1b4006a2f2693d0d19154728c4e2abcea563c
Lorenzo Colombini <lorenzinco23@gmail.com>
2024-12-04 21:49:17 +0100

    Made functions and variable names more clear. Also added documentation on dataset creation

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ DeRuster is proudly powered by [Compy](https://github.com/Etto48/compy), please 
 The project's scripts need a folder named "data", inside this folder divide the datasets into two other folders: test and training.
 The training and validation sets are split automatically from the test folder inside two subsets, which the validation is TRAIN_SIZE/100*VALIDATION_SPLIT, validation split is set at 20%, it can be modified inside the dataset.py file.
 
+
 ## Running the model
 This project makes use of python scripts, to run the model on a given binary please type in terminal:
 ```sh
@@ -21,4 +22,15 @@ run <binary_path>
 To retrain the model type:
 ```sh
 train
+```
+
+## Dataset
+A dataset generator is provided, to generate functions and variables inside the data folder please type in terminal:
+```sh
+generate --size <n-programs>
+```
+
+To compile such dataset into the asm folder please type in terminal:
+```sh
+compile
 ```
--- deruster/scripts/generate.py
+++ deruster/scripts/generate.py
@@ -4,18 +4,23 @@ import argparse
 import os
 from pathlib import Path
 
+functionnum = 0
+varnum = 0
+
 def get_random_string(length):
     letters = string.ascii_lowercase
     result_str = ''.join(random.choice(letters) for i in range(length))
     return result_str
 
 def generate_arithmetic_operation():
+    global functionnum
+    global varnum
     a = random.randint(0, 100)
     b = random.randint(1, 100)
     op_code = random.randint(0, 3)
 
     # generate a random string
-    function_name = get_random_string(10)
+    function_name = f"function_{functionnum}"
 
     fuction_code = f"""
     fn {function_name}(a: i32, b: i32) -> i32 {{
@@ -23,30 +28,38 @@ def generate_arithmetic_operation():
     }}\n
     """
     function_call = f"{function_name}({a}, {b});\n"
+    functionnum += 1
     return fuction_code, function_call
 
 def generate_variables():
+    global functionnum
+    global varnum
     a = random.randint(0, 100)
     b = random.randint(1, 100)
-    variable_name_a = get_random_string(10)
-    variable_name_b = get_random_string(10)
+
+    variable_name_a = f"var_{varnum}"
+    variable_name_b = f"var_{varnum+1}"
+
+    varnum += 2
 
 
     return f"let {variable_name_a} = {a};\nlet {variable_name_b} = {b};\n"
 
 def generate_socket_operation():
+    global functionnum
+    global varnum
     op_code = random.randint(0, 3)
-    function_name = get_random_string(10)
-    socket_name = get_random_string(10)
+    function_name = f"function_{functionnum}"
     function_imports = f"use std::net::UdpSocket;"
     fuction_code = f"""
     fn {function_name}() {{
         let mut buffer = [0; 10];
-        let {socket_name} = UdpSocket::bind("0.0.0.0:34254").expect("couldn't bind to address");
-        {socket_name}.recv_from(&mut buffer).expect("didn't receive data");
+        let socket = UdpSocket::bind("0.0.0.0:34254").expect("couldn't bind to address");
+        socket.recv_from(&mut buffer).expect("didn't receive data");
     }}\n
     """
     function_call = f"{function_name}();\n"
+    functionnum += 1
     return function_imports, fuction_code, function_call
 
 
@@ -54,14 +67,16 @@ def get_project_root():
     return Path(__file__).parent.parent.parent
 
 def generate_struct():
+    global functionnum
+    global varnum
 
-    struct_name = get_random_string(10)
+    struct_name = f"struct_{varnum}"
     struct_code = f"""struct {struct_name} {{"""
-    struct_declaration = f"let {get_random_string(10)} = {struct_name} {{"
+    struct_declaration = f"let var_{varnum} = {struct_name} {{"
     fields = random.randint(1, 10)
-    for _ in range(fields):
+    for i in range(fields):
         field_type = random.choice(["i32", "i64", "f32", "f64", "String"])
-        field_name = get_random_string(10)
+        field_name = "field_" + str(i+1)
         struct_code += f"\n    {field_name}: {field_type},"
         if field_type == "String":
             struct_declaration += f"\n    {field_name}: String::from(\"{get_random_string(10)}\"),"
@@ -73,33 +88,41 @@ def generate_struct():
             struct_declaration += f"\n    {field_name}: {random.uniform(0, 100)},"
         elif field_type == "f64":
             struct_declaration += f"\n    {field_name}: {random.uniform(0, 100)},"
-    struct_declaration += "\n};"
+    struct_declaration += "\n};\n"
     struct_code += "\n}\n"
+    varnum += 1
     return struct_code,struct_declaration
 
 def generate_enum():
-    enum_name = get_random_string(10)
+    global functionnum
+    global varnum
+    enum_name = f"enum_{varnum}"
     enum_code = f"""enum {enum_name} {{"""
-    enum_declaration = f"let {get_random_string(10)} = {enum_name}::"
+    enum_declaration = f"let var_{varnum} = {enum_name}::"
     variants = random.randint(1, 10)
     enum_fields = []
-    for _ in range(variants):
-        enum_field = get_random_string(10) 
+    for i in range(variants):
+        enum_field = f"variant_{i+1}"
         enum_fields.append(enum_field)
 
     for field in enum_fields:
         enum_code += f"\n    {field},"
-    enum_code += "\n}"
-    enum_declaration += f"{random.choice(enum_fields)};"
+    enum_code += "\n}\n"
+    enum_declaration += f"{random.choice(enum_fields)};\n"
+    varnum += 1
     return enum_code,enum_declaration
 
 def generate_dataset():
+    global functionnum
+    global varnum
     argparser = argparse.ArgumentParser()
     argparser.add_argument("--size", type=int, default=1000)
     argparser.add_argument("--train", type=bool, default=True)
     args = argparser.parse_args()
     size = args.size
     for i in range(size):
+        varnum = 0
+        functionnum = 0
         program_imports = ""
         function_declarations = ""
         main_code = "pub fn main() {\n"
